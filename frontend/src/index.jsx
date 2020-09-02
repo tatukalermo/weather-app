591fa7f3ce06716038e2619add8d16434a872d03
TatuKalermo <tatu.prayer@gmail.com>
2020-09-02 14:54:22 +0300

    Added more statistics and correct icons from the API

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -21,6 +21,8 @@ class Weather extends React.Component {
     this.state = {
       icon: '',
       temp: '',
+      humidity: '',
+      pressure: '',
       timeStamp: '',
       location: 'Helsinki',
       error: '',
@@ -37,8 +39,10 @@ class Weather extends React.Component {
       console.log('Weather data:', weatherData)
       this.setState(
         {
-          icon: weatherData.weather[0].icon.slice(0, -1),
+          icon: weatherData.weather[0].icon,
           temp: weatherData.main.temp,
+          humidity: weatherData.main.humidity,
+          pressure: weatherData.main.pressure,
           updatedAt: new Date().toISOString(),
           error: '',
         });
@@ -48,15 +52,17 @@ class Weather extends React.Component {
   }
 
   render() {
-    const { icon, temp, location, updatedAt } = this.state;
+    const { icon, temp, humidity, pressure, location, updatedAt } = this.state;
 
     return (
       <div>
         <div className="icon">
           <h2>Current weather in {location}</h2>
-          {icon && <img width={200} height={200} alt="weather_icon" src={`/img/${icon}.svg`} />}
+          {icon && <img width={100} height={100} alt="weather_icon" src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />}
           {updatedAt && <p>{updatedAt}</p>}
-          {temp && <p>{temp}</p>}
+          {temp && <p>Temperature: {temp}ºC</p>}
+          {humidity && <p>Humidity: {humidity}</p>}
+          {pressure && <p>Air pressure: {pressure}</p>}
           <button onClick={() => this.getWeather()}>Update</button>
         </div>
       </div>
