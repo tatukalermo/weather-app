2e6b82160004dc336c806381358f8036e3295cc1
TatuKalermo <tatu.prayer@gmail.com>
2020-09-08 14:08:16 +0300

    Added unit test suite for Forecast component

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
 import { Weather } from "./components/Weather";
+import { Forecast } from "./components/Forecast";
 
 describe("Weather Component", () => {
   const data = {
@@ -25,8 +26,32 @@ describe("Weather Component", () => {
     expect(wrapper.find("h1")).to.have.length(1);
   });
 
-  it("Renders 3 <p>", () => {
+  it("Renders 5 <p>", () => {
     const wrapper = shallow(<Weather data={data} />);
-    expect(wrapper.find("h1")).to.have.length(1);
+    expect(wrapper.find("p")).to.have.length(5);
+  });
+});
+
+describe("Forecast Component", () => {
+  const data = {
+    icon: "04d",
+    temp: 15,
+    time: "17:00",
+    icon2: "04d",
+    temp2: 14,
+    time2: "20:00",
+    icon3: "04d",
+    temp3: 13,
+    time3: "23:00",
+  };
+
+  it("Renders 3 <img>", () => {
+    const wrapper = shallow(<Forecast data={data} />);
+    expect(wrapper.find("img")).to.have.length(3);
+  });
+
+  it("Renders 6 <p>", () => {
+    const wrapper = shallow(<Forecast data={data} />);
+    expect(wrapper.find("p")).to.have.length(6);
   });
 });
