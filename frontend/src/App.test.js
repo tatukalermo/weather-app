5134a4102cd56d27723f168630939c5fa9442567
TatuKalermo <tatu.prayer@gmail.com>
2020-09-08 13:35:14 +0300

    Added unit test suite for weather component

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,32 @@
 /* eslint-disable no-undef */
 /* eslint-disable semi */
 /* eslint-disable react/jsx-filename-extension */
-describe("Something", () => {
-  it("renders the Weather wrapper ", () => {
-    const numb = 1;
-    expect(numb).equal(1);
+import React from "react";
+import { Weather } from "./components/Weather";
+
+describe("Weather Component", () => {
+  const data = {
+    icon: "04d",
+    temp: 17,
+    humidity: 97,
+    pressure: 1002,
+    weather: "rain",
+    updatedAt: "21:00",
+    location: "London",
+  };
+
+  it("Renders 5 <img>", () => {
+    const wrapper = shallow(<Weather data={data} />);
+    expect(wrapper.find("img")).to.have.length(5);
+  });
+
+  it("Renders 1 <h1>", () => {
+    const wrapper = shallow(<Weather data={data} />);
+    expect(wrapper.find("h1")).to.have.length(1);
+  });
+
+  it("Renders 3 <p>", () => {
+    const wrapper = shallow(<Weather data={data} />);
+    expect(wrapper.find("h1")).to.have.length(1);
   });
 });
